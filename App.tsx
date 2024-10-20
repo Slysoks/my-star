62613d9e483f00d4d450d944a9d0e683d33508a5
SupRMinecraftien <superminecraftien.levrai@gmail.com>
2024-10-21 01:28:24 +0200

    create new barnch

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ const App = () => {
       flex: 1,
       backgroundColor: Colors.light,
     }}>
-      <Text>App</Text>
+      <Text>Selector branch !</Text>
       <View style={styles.navigationContainer}>
         <NavigationContainer>
           <TabGroup />
