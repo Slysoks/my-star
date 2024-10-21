81764ea926c2e4871e9c924758f00acabc480827
SupRMinecraftien <superminecraftien.levrai@gmail.com>
2024-10-22 00:28:27 +0200

    Refactor code structure and add LinePicture provider

--- App.tsx
+++ App.tsx
@@ -1,19 +1,32 @@
-import { Text, Platform, SafeAreaView, StyleSheet, View } from 'react-native';
-import { useEffect } from 'react';
+import { Text, Platform, SafeAreaView, StyleSheet, View, Image } from 'react-native';
+import { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home, Horaires } from '@/views';
 import { Colors } from 'consts';
-import { NextBus } from '@/providers/star';
+import { LinePicture } from '@/providers/star';
 
 const App = () => {
+  const [image, setImage] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await LinePicture({ lineId: '0003', resolution: 1 });
+      setImage(data.image.url ? data.image.url : undefined);
+    };
+
+    fetchData();
+  }, []);
   return (
     <SafeAreaView style={{
       paddingTop: Platform.OS == 'android' ? 25 : 0,
       flex: 1,
       backgroundColor: Colors.light,
     }}>
-      <Home />
+      <Image
+        source={{ uri: image }}
+        style={{ width: 100, height: 100 }}
+      />
       <View style={styles.navigationContainer}>
         <NavigationContainer>
           <TabGroup />
--- src/providers/star.ts
+++ src/providers/star.ts
@@ -1,3 +1,4 @@
+// NextBus provider
 interface NextBusProps {
   lineId: number | undefined;
   lineName?: string | undefined;
@@ -11,11 +12,10 @@ interface NextBusProps {
   limit?: number;
 }
 
-// Create a new provider
 const NextBus = async (props: NextBusProps) => {
   // Get the props
   const { lineId, lineName, destinationId, destinationName, stopId, stopName, rideId, busId, timezone, limit=20 } = props;
-  const uri = 'https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-bus-circulation-passages-tr/records?';
+  const url = 'https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-bus-circulation-passages-tr/records?';
   
   const params = {
     lineId: lineId,
@@ -30,19 +30,92 @@ const NextBus = async (props: NextBusProps) => {
     limit: limit,
   };
 
+  // Build the query string
   const args = Object.keys(params)
     .filter((key) => params[key as keyof typeof params] !== undefined)
     .map((key) => key + '=' + params[key as keyof typeof params])
     .join('&');
 
-  const url = uri + args;
+  const uri = url + args;
+
   // Fetch the data
-  const response = await fetch(url);
+  const response = await fetch(uri);
   const data = await JSON.parse(await response.text());
 
   return (data);
 };
 
+// Global line provider
+interface LineListProps {
+  lineId?: number | undefined;
+  lineName?: string | undefined;
+  family?: string | undefined;
+  limit?: number;
+}
+
+const LineList = async (props: LineListProps) => {
+  // Get the props
+  const { lineId, lineName, family, limit=-1 } = props;
+  const url = 'https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-bus-topologie-lignes-td/records?';
+  
+  const params = {
+    lineId: lineId,
+    lineName: lineName,
+    family: family,
+    limit: limit,
+  };
+
+  // Build the query string
+  const args = Object.keys(params)
+    .filter((key) => params[key as keyof typeof params] !== undefined)
+    .map((key) => key + '=' + params[key as keyof typeof params])
+    .join('&');
+
+  const uri = url + args;
+
+  // Fetch the data
+  const response = await fetch(uri);
+  const data = await JSON.parse(await response.json());
+
+  return (data);
+};
+
+// Line picture provider
+interface LinePictureProps {
+  lineId: string;
+  resolution: number;
+}
+
+const LinePicture = async (props: LinePictureProps) => {
+  // Get the props
+  const { lineId, resolution=1 } = props;
+  if (resolution !== 0 && resolution !== 1) return null;
+  const uri = `https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-bus-lignes-pictogrammes-dm/records?limit=5&refine=idligne:${lineId}`;
+  
+  // Fetch the data
+  const response = await fetch(uri);
+  const data = await response.json();
+  console.log(data);
+
+  if (data.results.length === 0) return null;
+  if (data.results.length === 1) return data.results[0];
+
+  const selectedPicture = data.results.reduce((prev: any, curr: any) => {
+    const prevArea = prev.taille;
+    const currArea = curr.taille;
+
+    if (resolution === 0) {
+      return currArea < prevArea ? curr : prev;
+    } else {
+      return currArea > prevArea ? curr : prev;
+    }
+  });
+
+  return selectedPicture;
+};
+
 export {
   NextBus,
+  LineList,
+  LinePicture,
 };
