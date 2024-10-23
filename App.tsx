d5efd8c284187b1d5bb1985dd02908cf42f75d15
SupRMinecraftien <superminecraftien.levrai@gmail.com>
2024-10-23 18:51:53 +0200

    refactor: Update App component to use NextBus provider for fetching data

--- App.tsx
+++ App.tsx
@@ -1,18 +1,19 @@
-import { Text, Platform, SafeAreaView, StyleSheet, View, Image } from 'react-native';
+import { Text, Platform, SafeAreaView, StyleSheet, View, Image, ScrollView } from 'react-native';
 import { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home, Horaires } from '@/views';
 import { Colors } from 'consts';
-import { LinePicture } from '@/providers/star';
+import { NextBus } from '@/providers/star';
 
 const App = () => {
   const [image, setImage] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await LinePicture({ lineId: '0003', resolution: 1 });
-      setImage(data?.image?.url ?? undefined);
+      const data = await NextBus({ lineId: '0003' });
+      console.log(data);
+      setImage(JSON.stringify(data));
     };
 
     fetchData();
@@ -23,15 +24,14 @@ const App = () => {
       flex: 1,
       backgroundColor: Colors.light,
     }}>
-      <Image
-        source={{ uri: image }}
-        style={{ width: 100, height: 100 }}
-      />
-      <View style={styles.navigationContainer}>
-        <NavigationContainer>
-          <TabGroup />
-        </NavigationContainer>
-      </View>
+      <ScrollView style={styles.navigationContainer}>
+        <Text>
+          { image }
+        </Text>
+      </ScrollView>
+      <NavigationContainer>
+        <TabGroup />
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
@@ -51,6 +51,7 @@ const styles = StyleSheet.create({
   navigationContainer: {
     position: 'absolute',
     width: '100%',
+    height: 150,
     bottom: 0,
   },
 });
