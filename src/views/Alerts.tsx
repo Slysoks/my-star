b14a12b6162d3fa4f033a637be23158f44820190
SupRMinecraftien <superminecraftien.levrai@gmail.com>
2024-12-26 15:45:05 +0100

    feat: add Alerts component with basic layout

---
+++ src/views/Alerts.tsx
@@ -0,0 +1,18 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+
+const Alerts = ({ navigation }: any) => {
+  return (
+    <View>
+      <View>
+        <Text>Alertes</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Alerts;
