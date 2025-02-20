ffc159ff4a3fe17360d6a703ea3f5b57c170cc23
SupRMinecraftien <superminecraftien.levrai@gmail.com>
2025-02-21 00:01:15 +0100

    feat: implement NativeList and NativeListItem components for improved list rendering in Horaires view

---
+++ src/components/lists.tsx
@@ -0,0 +1,126 @@
+import React, { ReactNode } from "react";
+import { Text, Pressable } from "react-native";
+import Reanimated, {
+  type AnimatedProps,
+  EntryExitTransition,
+  LinearTransition,
+} from "react-native-reanimated";
+import { CircleHelp } from "lucide-react-native";
+
+type EntryOrExitLayoutType = NonNullable<AnimatedProps<{}>["entering"]>;
+
+interface NativeListProps {
+  theme: any;
+  icon?: ReactNode;
+  label: string;
+  leading?: ReactNode;
+  trailing?: ReactNode;
+  animated?: boolean;
+  entering?: EntryOrExitLayoutType;
+  children?: ReactNode;
+}
+
+const NativeList: React.FC<NativeListProps> = ({
+  theme,
+  label,
+  animated,
+  entering,
+  children,
+}) => {
+  return (
+    <Reanimated.View>
+      <Text style={{ color: theme.colors.text }}>{label}</Text>
+      <Reanimated.View
+        style={{
+          backgroundColor: theme.colors.card,
+          marginHorizontal: 5,
+          borderRadius: 10,
+          padding: 10,
+          borderWidth: 2,
+          borderColor: theme.colors.border,
+          overflow: "hidden",
+        }}
+      >
+        {React.Children.map(children, (child, index) => (
+          <>
+            <Reanimated.View style={{ overflow: "hidden" }}>
+              {child}
+            </Reanimated.View>
+            {index < React.Children.count(children) - 1 && (
+              <Reanimated.View
+                style={{
+                  height: 1,
+                  backgroundColor: theme.colors.border,
+                  marginVertical: 8,
+                }}
+              />
+            )}
+          </>
+        ))}
+      </Reanimated.View>
+    </Reanimated.View>
+  );
+};
+
+interface NativeListItemProps {
+  theme: any;
+  icon?: ReactNode;
+  iconColor?: string;
+  label: string;
+  sub: string;
+  trailing?: ReactNode;
+  animated?: boolean;
+  entering?: EntryOrExitLayoutType;
+}
+
+const NativeListItem: React.FC<NativeListItemProps> = ({
+  theme,
+  icon = <CircleHelp color={"#000000"} />,
+  iconColor = "#ffffff",
+  label,
+  sub,
+  trailing,
+  animated = false,
+  entering = EntryExitTransition,
+}) => {
+  return (
+    <Reanimated.View
+      entering={entering}
+      style={{ flexDirection: "row", position: "relative" }}
+    >
+      <Reanimated.View
+        style={{
+          backgroundColor: iconColor,
+          borderRadius: 10,
+          padding: 10,
+          borderColor: theme.colors.border,
+        }}
+      >
+        {icon}
+      </Reanimated.View>
+      <Reanimated.View
+        style={{
+          marginLeft: 5,
+        }}
+      >
+        <Text>{label}</Text>
+        <Text>{sub}</Text>
+      </Reanimated.View>
+      <Reanimated.View
+        style={{
+          position: "absolute",
+          right: 0,
+          top: 0,
+          bottom: 0,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {trailing}
+        <Text>Test</Text>
+      </Reanimated.View>
+    </Reanimated.View>
+  );
+};
+
+export { NativeList, NativeListItem };
--- src/views/Horaires/Horaires.tsx
+++ src/views/Horaires/Horaires.tsx
@@ -5,15 +5,23 @@ import {
   ScrollView,
   StyleSheet,
   Pressable,
+  Image,
 } from "react-native";
-import { useState, useEffect } from "react";
-import { NextBus } from "@/providers/star";
+import { useEffect, useState } from "react";
+import { FadeInDown, Easing } from "react-native-reanimated";
+import { LineList, NextBus } from "@/providers/star";
+import { NativeList, NativeListItem } from "@/components/lists";
+import { BusFront, TramFront } from "lucide-react-native";
+import { useTheme } from "@react-navigation/native";
 
 const Horaires = ({ navigation }: any) => {
+  const theme = useTheme();
+
   const [stopName, setStopName] = useState<string>("Loges");
   const [line, setLine] = useState<string>("C1");
   const [destination, setDestination] = useState<string>("Saint-Grégoire");
   const [buses, setBuses] = useState<any[]>([]);
+  const [lineColor, setLineColor] = useState<string>("");
 
   function search() {
     NextBus({
@@ -29,6 +37,18 @@ const Horaires = ({ navigation }: any) => {
         console.error(error);
       });
   }
+
+  useEffect(() => {
+    async function fetchLineColor() {
+      try {
+        const data = await LineList({ lineName: line });
+        setLineColor(data.results[0].couleur);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchLineColor();
+  }, [line]);
 
   return (
     <ScrollView>
@@ -64,23 +84,29 @@ const Horaires = ({ navigation }: any) => {
           </Text>
         </Pressable>
       </View>
+
       <View>
-        {buses.length > 0 &&
-          buses.map((bus, index) => (
-            <Pressable key={index} style={styles.card} android_ripple={{ color: "grey" }}>
-              <Text>
-                {bus.nomcourtligne} - {bus.destination}
-              </Text>
-              <Text>
-                {new Date(bus.arrivee).toLocaleTimeString("fr-FR", {
+        {buses.length > 0 && (
+          <NativeList theme={theme} label="Liste des arrêts">
+            {buses.map((bus, index) => (
+              <NativeListItem
+                icon={<BusFront color={"#000"} />}
+                iconColor={"#95c11e"}
+                key={index}
+                theme={theme}
+                label={new Date(bus.arrivee).toLocaleTimeString("fr-FR", {
                   hour: "2-digit",
                   minute: "2-digit",
-                  second: "2-digit",
                   hour12: false,
                 })}
-              </Text>
-            </Pressable>
-          ))}
+                sub={bus.destination}
+                entering={FadeInDown.springify()
+                  .easing(Easing.elastic(1))
+                  .delay(100 * index)}
+              />
+            ))}
+          </NativeList>
+        )}
       </View>
     </ScrollView>
   );
